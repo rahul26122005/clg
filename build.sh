7117c78e83fe8cfc569a33158854a589eb218717
rahul26122005 <rahulr26122005@gmail.com>
2024-06-23 22:13:22 +0530

    a

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-#bulid the project
-echo "Build the project..."
-python -m pip install -r requirements.txt
+# Check if virtual environment exists
+if [ ! -d "venv" ]; then
+  # Create virtual environment
+  python3.8 -m venv venv
+fi
 
-echo "Made Migration..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+# Activate virtual environment
+source venv/bin/activate
 
+# Verify Python version
+python --version
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
 echo "Collect Static..."
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput
+
+# Run migrations
+python manage.py migrate
