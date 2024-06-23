f01393051ed22168aeda4b83f84175e48eec652f
rahul26122005 <rahulr26122005@gmail.com>
2024-06-23 22:01:44 +0530

    changes on vercel&build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 #bulid the project
 echo "Build the project..."
-python3.8.6rc1 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 echo "Made Migration..."
-python3.8.6rc1 manage.py makemigrations --noinput
-python3.8.6rc1 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.8.6rc1 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
