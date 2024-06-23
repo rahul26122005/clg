f4b449ba1f7a4675d60e4c071830af407c645509
rahul26122005 <rahulr26122005@gmail.com>
2024-06-23 21:50:54 +0530

    changes on python-version

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 #bulid the project
 echo "Build the project..."
-python3.7 -m pip install -r requirements.txt
+python3.8 -m pip install -r requirements.txt
 
 echo "Made Migration..."
-python3.7 manage.py makemigrations --noinput
-python3.7 manage.py migrate --noinput
+python3.8 manage.py makemigrations --noinput
+python3.8 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.7 manage.py collectstatic --noinput --clear
+python3.8 manage.py collectstatic --noinput --clear
--- clg_projects/settings.py
+++ clg_projects/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +84,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASE[ 'default'] = dj_database_url.config()
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config()
+else:
+    print("WARNING:root:No DATABASE_URL environment variable set, and so no databases setup")
 
 
 # Password validation
