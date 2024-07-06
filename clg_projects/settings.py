03cd7a617a798f64cda81fbfdae5180e4fcad8ae
rahul26122005 <rahulr26122005@gmail.com>
2024-07-06 16:36:02 +0530

    new modification

--- clg_projects/settings.py
+++ clg_projects/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-from decouple import config
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +39,7 @@ CSRF_COOKIE_SECURE = True
 
 INSTALLED_APPS = [
     'university',
+    'django_extensions',
     'pandas',
     'openpyxl',
     'django.contrib.admin',
